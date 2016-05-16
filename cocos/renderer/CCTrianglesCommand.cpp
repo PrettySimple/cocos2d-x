8a20a6e792bc9e558d29767dea168d9300b9c3e3
Frederic Heintz <frederic@prettysimplegames.com>
2016-05-16 14:20:02 +0200

    update batching of triangleCommand / batch with same programState

--- cocos/renderer/CCTrianglesCommand.cpp
+++ cocos/renderer/CCTrianglesCommand.cpp
@@ -77,10 +77,16 @@ TrianglesCommand::~TrianglesCommand()
 
 void TrianglesCommand::generateMaterialID()
 {
-    
     if(_glProgramState->getUniformCount() > 0)
     {
-        _materialID = Renderer::MATERIAL_ID_DO_NOT_BATCH;
+//      _materialID = Renderer::MATERIAL_ID_DO_NOT_BATCH;   // cocos would not batch
+
+        // adding the programmState in the Hash, if programm states are the same then uniforms also ...
+        uint64_t temp = (uintptr_t)_glProgramState;
+        int progState = ((temp >> 32) ^ temp);
+        int glProgram = (int)_glProgramState->getGLProgram()->getProgram();
+        int intArray[5] = { glProgram, (int)_textureID, (int)_blendType.src, (int)_blendType.dst, progState };
+        _materialID = XXH32((const void*)intArray, sizeof(intArray), 0);
     }
     else
     {
