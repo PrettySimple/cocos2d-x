4e1f254a3e45f9b6e9bc74af420393d6888619dc
Mathieu Garaud <mathieu.garaud@gmail.com>
2017-07-05 15:02:21 +0200

    Remove left shift of negative warning that is UB

--- cocos/2d/CCNode.cpp
+++ cocos/2d/CCNode.cpp
@@ -269,7 +269,7 @@ void Node::setLocalZOrder(int z)
 /// used internally to alter the zOrder variable. DON'T call this method manually
 void Node::_setLocalZOrder(int z)
 {
-    _localZOrderAndArrival = (static_cast<std::int64_t>(z) << 32) | (_localZOrderAndArrival & 0xffffffff);
+    _localZOrderAndArrival = (static_cast<std::uint64_t>(z) << 32) | (_localZOrderAndArrival & 0xffffffff);
     _localZOrder = z;
 }
 
